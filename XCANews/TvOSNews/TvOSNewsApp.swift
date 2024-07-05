c307ebe0855069a869b5e95d921614941a0f4859
athata1 <akhil.thata@gmail.com>
2024-07-05 17:26:17 -0400

    Fixed initialization

---
+++ XCANews/TvOSNews/TvOSNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TvOSNewsApp.swift
+//  TvOSNews
+//
+//  Created by Akhil Thata on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct TvOSNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
